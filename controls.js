adb7dfa1b54d94591728afab0be64971a766ae26
Marwen-Hammami <hammamimarwen.d@gmail.com>
2022-07-04 10:55:04 +0200

    second commit

---
+++ controls.js
@@ -0,0 +1,57 @@
+class Controls {
+  constructor(controlType) {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    switch (controlType) {
+      case "KEYS":
+        this.#addKeyboardListeners();
+        break;
+      case "DUMMY":
+        this.forward = true;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+      //   console.table(this);
+    };
+    document.onkeyup = (event2) => {
+      switch (event2.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      //   console.table(this);
+    };
+  }
+}
